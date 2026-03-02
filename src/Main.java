61bace57d5b96f8ddbd6b0332e6e3a6e9bb12fa5
ThiagoWill19 <thiagopompeu19@gmail.com>
2026-03-02 14:26:49 -0300

    Criado DevService para lógica de negócio

--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,49 @@ import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 import br.com.dio.desafio.repositories.BootcampRepository;
 import br.com.dio.desafio.repositories.DevRepository;
+import br.com.dio.desafio.services.DevService;
+
+import java.util.ArrayList;
 
 
 public class Main {
 
     public static void main(String[] args) {
 
+        DevService devService = new DevService();
 
         Dev dev = new Dev();
-        dev.setNome("Thiago");
+        dev.setNome("João");
 
         Conteudo conteudo = new Mentoria();
         conteudo.setTitulo("Mentoria Java");
         conteudo.setDescricao("Descrição Mentoria Java");
 
-
         Bootcamp bootcamp = new Bootcamp();
-
         bootcamp.setNome("Bootcamp Java DIO");
         bootcamp.setDescricao("Descrição Bootcamp Java DIO");
         bootcamp.getConteudos().add(conteudo);
 
-        bootcamp.getDevsInscritos().add(dev);
-        dev.getConteudosInscritos(bootcamp.getConteudos());
 
-        BootcampRepository.save(bootcamp);
-        DevRepository.save(dev);
+        try {
+            dev = devService.newDev(dev);
+            BootcampRepository.save(bootcamp);
+            devService.subscribeBootcamp(1,1);
 
-        System.out.println("===== Bootcamps =====");
-        System.out.println(BootcampRepository.findAll());
+            devService.progress(dev.getId());
+
+
+        }catch (Exception e){
+            System.out.println("Exception --> "+ e.getMessage());
+            return;
+        }
+
 
         System.out.println("===== Devs =====");
         System.out.println(DevRepository.findAll());
 
+        System.out.println("===== Bootcamps ====");
+        System.out.println(BootcampRepository.findAll());
 
 
     }
---
+++ src/br/com/dio/desafio/services/DevService.java
@@ -0,0 +1,102 @@
+package br.com.dio.desafio.services;
+
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.repositories.BootcampRepository;
+import br.com.dio.desafio.repositories.DevRepository;
+
+import java.util.ArrayList;
+
+public class DevService {
+
+    public Dev newDev(Dev dev) throws Exception {
+
+        if(dev != null){
+
+            if(dev.getNome() == null || dev.getNome().trim().isEmpty()){
+                throw new Exception("O campo nome é obrigatório");
+            }
+
+            return DevRepository.save(dev);
+
+        }else{
+            throw new Exception("Dev não pode ser null");
+        }
+    }
+
+
+    public Dev findById(int id) throws Exception{
+
+        return DevRepository.findByID(id).orElseThrow(() -> new Exception("Não existe um dev com ID informado"));
+    }
+
+
+    public ArrayList<Dev> findAll(){
+        return (ArrayList<Dev>) DevRepository.findAll();
+    }
+
+
+    public boolean deleteById(int id){
+        return DevRepository.deleteById(id);
+    }
+
+
+    public void subscribeBootcamp(int devId, int bootcampId) throws Exception {
+
+        Dev dev;
+        Bootcamp bootcamp;
+
+        if(devId > 0){
+            dev = DevRepository.findByID(devId)
+                    .orElseThrow(() -> new Exception("Dev não encontrado com ID informado"));
+
+            if(bootcampId > 0){
+                bootcamp = BootcampRepository.findById(bootcampId)
+                        .orElseThrow(() -> new Exception("Bootcamp não encontrado com ID informado"));
+
+                if(!bootcamp.getDevsInscritos().contains(dev)){
+
+                    dev.setConteudosInscritos(bootcamp.getConteudos());
+                    bootcamp.getDevsInscritos().add(dev);
+
+
+                    try{
+
+                        DevRepository.update(dev);
+                        BootcampRepository.update(bootcamp);
+
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }else{
+                    throw new Exception("Esse Dev já se inscreveu nesse Bootcamp");
+                }
+
+
+            }else{
+                throw new Exception("bootcampId inválido");
+            }
+
+        }else{
+            throw new Exception("devId inválido");
+        }
+
+    }
+
+
+    public void progress(int devId) throws Exception{
+
+        Dev dev = findById(devId);
+        dev.progredir();
+        DevRepository.update(dev);
+
+    }
+
+
+    public double calculateXP(int devId) throws Exception{
+        Dev dev = findById(devId);
+        return dev.calcularTotalXp();
+    }
+
+}
